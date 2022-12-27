7f6ebdbbf335e28d8c5cab2cdaaa747ccbb3841e
eknaian <eknaian@gmail.com>
2022-12-27 09:45:56 -0500

    script almost complete

--- client/script.js
+++ client/script.js
@@ -17,3 +17,41 @@ function loader(element) {
     }
   }, 300);
 }
+
+function typeText(element, text) {
+  let index = 0;
+
+  let interval = setInterval(() => {
+    if (index < text.length) {
+      element.innerHTML += text.chartAt(index);
+      index++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 20);
+}
+
+function generateUniqueId() {
+  const timeStamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+
+  return `id-${timeStamp}-${hexadecimalString}`;
+}
+
+function chatStripe(isAi, value, uniqueId) {
+  return `
+    <div class="wrapper ${isAi && "ai"}">
+      <div class="chat">
+        <div class="profile">
+          <img src="${isAi ? bot : user}" alt="${isAi ? "bot" : "user"}" />
+        </div>
+        <div class="message" id=${uniqueId}>${value}</div>
+      </div>
+    </div>
+    `;
+}
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+};
